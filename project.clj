4aa43c23da30706c2b92c2de10c7fcdde9531e2b
Steven Deobald <steven@deobald.ca>
2012-01-06 17:33:17 -0700

    file formats

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
-                           [clj-stacktrace "0.2.4"]]
+                           [clj-stacktrace "0.2.4"]
+                           [clj-time "0.3.4"]]
             :main haystack.server)
 
--- src/haystack/views/index.clj
+++ src/haystack/views/index.clj
@@ -1,9 +1,13 @@
 (ns haystack.views.index
   (:require [haystack.views.common :as common]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clj-time.core :as time]
+            [clj-time.format :as time-format])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]
         [hiccup.form-helpers :only [form-to file-upload text-field submit-button]]))
+
+(def upload-label "file:")
 
 (defpage "/" []
   (common/layout
@@ -11,7 +15,7 @@
    [:br]
 
    [:form {:action "/drop" :method "post" :enctype "multipart/form-data"} 
-    (file-upload "file:")
+    (file-upload upload-label)
     (submit-button "drop")]
    [:br]
 
@@ -19,14 +23,22 @@
             (text-field "what")
             (submit-button "find"))))
 
-(defn upload-file [data]
-  (io/copy (:tempfile data) (io/file "file.out")))
+(defn timestamp []
+  (time-format/unparse (time-format/formatters :basic-date-time-no-ms) (time/now)))
+
+(defn upload-file
+  "data will be in format: {:size 179,
+                            :tempfile #,
+                            :content-type \"application/x-desktop\",
+                            :filename \"examples.desktop\"}"
+  [data]
+  (io/copy (:tempfile data) (io/file (str (timestamp) "-" (:filename data)))))
 
 (defpage [:post "/drop"] params
-  (let [data ((keyword "file:") params)]
+  (let [data ((keyword upload-label) params)
+        result (upload-file data)]
     (common/layout
-     [:pre 
-      ;;(str "zig: " (keys params))
-      ]
-     (upload-file data)
-     )))
+     [:div
+      [:div "Uploaded!"]
+      [:div [:a {:href "/"} "Go back?"]]])))
+
