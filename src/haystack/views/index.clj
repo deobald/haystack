19c81fa15c56ad8e1c6c84b56baabfef6c752c7b
Steven Deobald <steven@deobald.ca>
2012-01-23 23:23:56 -0600

    crappy find, /files dir

--- src/haystack/views/index.clj
+++ src/haystack/views/index.clj
@@ -1,6 +1,7 @@
 (ns haystack.views.index
   (:require [haystack.views.common :as common]
             [clojure.java.io :as io]
+            [clojure.java.shell :as shell]
             [clj-time.core :as time]
             [clj-time.format :as time-format])
   (:use [noir.core :only [defpage]]
@@ -42,3 +43,14 @@
      [:div
       [:div "Uploaded!"]
       [:div [:a {:href "/"} "Go back?"]]])))
+
+(defn find-files [what]
+  (shell/sh "find" "files" "-name" (str "'*" what "*'")))
+
+(defpage [:post "/find"] {what :what}
+  (let [results (find-files what)]
+    (common/layout
+     [:div
+      [:div "Found:"]
+      (for [r results]
+        [:div r])])))
