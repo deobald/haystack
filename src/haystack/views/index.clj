603b0bd96e6860c78c628bf1b4880680a90b93aa
Steven Deobald <steven@c42.in>
2012-04-09 22:46:16 +0530

    jiggery pokery

--- src/haystack/views/index.clj
+++ src/haystack/views/index.clj
@@ -4,18 +4,31 @@
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]
         [hiccup.page-helpers :only [image]]
-        [hiccup.form-helpers :only [form-to file-upload text-field submit-button]]))
+        [hiccup.form-helpers :only [form-to
+                                    file-upload
+                                    text-field
+                                    submit-button]]))
+
+(defn dropper []
+  [:div#dropper
+   [:form {:action "/drop" :method "post" :enctype "multipart/form-data"}
+    [:div (file-upload file/upload-label)]
+    [:div (submit-button {:class "btn primary"} "drop")]]])
+
+(defn finder []
+  [:div#finder
+   (form-to [:post "/find"]
+            [:div (text-field "what")]
+            [:div (submit-button {:class "btn primary"} "find")])])
+
+(defn file-list []
+  (for [f (file/ls)]
+      [:div.file f]))
 
 (defpage "/" []
   (layout/layout
    [:div
-    [:div#dropper
-     [:form {:action "/drop" :method "post" :enctype "multipart/form-data"}
-      [:div (file-upload file/upload-label)]
-      [:div (submit-button {:class "btn primary"} "drop")]]]
-    [:div#logo
-     (image "/img/haystack.jpg" "HAYSTACK!")]
-    [:div#finder
-     (form-to [:post "/find"]
-              [:div (text-field "what")]
-              [:div (submit-button {:class "btn primary"} "find")])]]))
+    (dropper)
+    (finder)]
+   [:div.file-list
+    (file-list)]))
