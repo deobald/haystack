4beaf3b6928ca6766339c2f405ccd3a95c9f1670
Steven Deobald <steven@c42.in>
2012-04-11 10:57:54 +0530

    interim commit. stealing jukebox js file-drop stuff.

--- src/haystack/views/layout.clj
+++ src/haystack/views/layout.clj
@@ -6,6 +6,11 @@
   (html5 
    [:head
     [:title "haystack"]
+    [:script {:src "/js/vendor/jquery-1.6.4.min.js"}]
+    [:script {:src "/js/vendor/underscore-min.js"}]
+    [:script {:src "/js/notifications.js"}]
+    [:script {:src "/js/uploader.js"}]
+    [:script {:src "/js/files.js"}]
     (include-css "/css/reset.css")
     (include-css "/css/haystack.css")]
    [:body
